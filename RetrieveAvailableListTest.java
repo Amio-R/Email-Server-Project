9c04490c39ae189db6dbb333499453224928ecc4
Amio Rahman <am4rahma@uwaterloo.ca>
2018-04-02 20:26:50 -0400

    Delete RetrieveAvailableListTest.java

--- RetrieveAvailableListTest.java
+++
@@ -1,6 +0,0 @@
-public class RetrieveAvailableListTest {
-    public static void main(String [] args){
-	int error = FileIO.retrieveAvailableList("_available3.txt");
-	System.out.println(Globals.availableList);
-    }
-}
